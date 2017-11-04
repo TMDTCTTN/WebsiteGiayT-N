db7ebad61e215e0d130bd93a5193cfd63f70e2ab
ptcamtu97 <ptcamtu97@gmail.com>
2017-11-04 21:21:26 +0700

    new

---
+++ WebsiteGiay/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteGiay.Models;
+
+namespace WebsiteGiay.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLGIAYEntities db = new QLGIAYEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        /*partial giày menu*/
+        public PartialViewResult _GiayMenuPartial()
+        {
+            var lstLoaiGiay = db.LOAIGIAYs.ToList();
+            return PartialView(lstLoaiGiay);
+        }
+        public PartialViewResult _NewGiayFooterPartial()
+        {
+            var lstHinhFooter = db.HINHSPs.Take(9).ToList();
+            return PartialView(lstHinhFooter);
+        }
+        
+        public PartialViewResult _GiayBBPartial()
+        {
+            var lstGiay = db.GIAYs.OrderBy(n => n.NgayCapNhat).Take(3).ToList();
+            return PartialView(lstGiay);
+        }
+        public PartialViewResult _GiayBupBePartial()
+        {
+            var lstGiay = db.GIAYs.Where(n => n.MaLoai == "GBB").Take(4).ToList();
+            return PartialView(lstGiay);
+        }
+        
+    }
+}
---
+++ WebsiteGiay/Models/AccountViewModels.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteGiay.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Tên đăng nhập")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} phải ít nhất {2} ký tự.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu mới")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Nhập lại mật khẩu")]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu không trùng khớp!")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Tên đăng nhập")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [Display(Name = "Nhớ mật khẩu?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Tên đăng nhập")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} phải ít nhất {2} ký tự.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Nhập lại mật khẩu")]
+        [Compare("Password", ErrorMessage = "Mật khẩu không trùng khớp!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
